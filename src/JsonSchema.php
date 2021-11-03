14d7a86e34eee258dc32c1f6913e847ca8ecf0a7
andrealvesabreu <aluisalves0@gmail.com>
2021-11-03 01:34:26 -0300

    Started

---
+++ src/JsonSchema.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+namespace Inspire\Validator;
+
+/**
+ *
+ * @author aalves
+ *        
+ */
+class JsonSchema extends \Inspire\Core\Utils\JsonValidator
+{
+}
+
+
---
+++ src/Variable.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types = 1);
+namespace Inspire\Validator;
+
+use Respect\Validation\ {
+    Validator,
+    Factory
+};
+use Respect\Validation\Exceptions\ComponentException;
+
+/**
+ *
+ * @author aalves
+ *        
+ */
+class Variable
+{
+
+    private static ?Validator $instance = null;
+
+    /**
+     * Create instance validator.
+     */
+    public static function create(): Validator
+    {
+        self::$instance = new Validator();
+        return self::$instance;
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     */
+    public function check($input): void
+    {
+        self::$instance->check($input);
+    }
+
+    /**
+     * Creates a new Validator instance with a rule that was called on the static method.
+     *
+     * @param mixed[] $arguments
+     *
+     * @throws ComponentException
+     */
+    public static function __callStatic(string $ruleName, array $arguments): Validator
+    {
+        return Variable::create()->__call($ruleName, $arguments);
+    }
+
+    /**
+     * Create a new rule by the name of the method and adds the rule to the chain.
+     *
+     * @param mixed[] $arguments
+     *
+     * @throws ComponentException
+     */
+    public function __call(string $ruleName, array $arguments): Validator
+    {
+        self::$instance->addRule(Factory::getDefaultInstance()->rule($ruleName, $arguments));
+        return self::$instance;
+    }
+}
+
